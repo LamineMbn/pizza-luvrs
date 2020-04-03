5bd4ec28707971292c745ae8b69981db1bc52ff2
mlaminebendib <mohamed-lamine.bendib@consulting-for.accor.com>
2020-04-03 19:39:44 +0200

    Add Redis

--- index.js
+++ index.js
@@ -5,7 +5,19 @@ const routes = require('./routes')
 
 async function startServer () {
   const server = Hapi.Server({
-    port: process.env.PORT || 3000
+    port: 3000,
+    cache: [
+      {
+        name: 'redis',
+        provider: {
+          constructor: require('@hapi/catbox-redis'),
+          options: {
+            partition: 'cache',
+            host: 'pizza-cluster.q0k72z.0001.euw3.cache.amazonaws.com'
+          }
+        }
+      }
+    ]
   })
 
   await plugins.register(server)
